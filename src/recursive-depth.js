e1f031ca6119a7e131d0b03ac6cb7210bb505cf3
badikgit <stas_yar@tut.by>
2022-04-30 16:58:53 +0300

    feat: task simple-chain completed

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,12 +13,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    calculateDepth(arr) {
+        throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+    }
 }
 
 module.exports = {
-  DepthCalculator
+    DepthCalculator
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,28 +5,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    chain: [],
+    getLength() {
+        return this.chain.length;
+    },
+    addLink(value) {
+        if (value == undefined) this.chain.push('null');
+        else this.chain.push(value);
+        return this;
+    },
+    removeLink(position) {
+        if (position > this.getLength() || isNaN(position) || (position <= 0) || (position % 1 > 0)) {
+            this.chain = [];
+            throw new Error(`You can't remove incorrect link!`);
+        }
+        this.chain.splice(position - 1, 1);
+        return this;
+    },
+    reverseChain() {
+        this.chain = this.chain.reverse();
+        return this;
+    },
+    finishChain() {
+        let chain1 = this.chain.join(' )~~( ');
+        chain1 = '( ' + chain1 + ' )'
+        this.chain = [];
+        return chain1;
+    }
 };
 
 module.exports = {
-  chainMaker
+    chainMaker
 };
